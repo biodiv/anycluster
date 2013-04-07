99bc00cd2c518fc870340f4e497386e0fab5cd24
biodiv <tom@eyesonic.net>
2013-04-07 21:20:31 +0200

    added getBounds view

--- anycluster/urls.py
+++ anycluster/urls.py
@@ -8,4 +8,6 @@ from django.conf import settings
 urlpatterns = patterns('',
     url(r'^grid/(\d+)/(\d+)/$', views.getGrid, name='getGrid'),
     url(r'^kmeans/(\d+)/(\d+)/$', views.getPins, name='getPins'),
+    url(r'^getbounds/$', views.getBounds, name='getBounds'),
+    url(r'^getbounds/(\d+)/$', views.getBounds, name='getBounds_wsrid'),
 )
