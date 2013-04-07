50d61b3dad0784b70c115ef3d7513e74b5ea70e8
biodiv <tom@eyesonic.net>
2013-04-07 21:20:13 +0200

    added getBounds view

--- anycluster/views.py
+++ anycluster/views.py
@@ -6,8 +6,9 @@ from django.utils import simplejson
 def getGrid(request, zoom, gridSize=256):
 
     clusterer = MapClusterer()
+    clustercells, filters = clusterer.getClusterParameters(request,int(zoom), int(gridSize))
 
-    grid = clusterer.gridCluster(request,int(zoom), int(gridSize))
+    grid = clusterer.gridCluster(clustercells,filters,int(zoom), int(gridSize))
     
     return HttpResponse(simplejson.dumps(
         grid
@@ -17,9 +18,22 @@ def getGrid(request, zoom, gridSize=256):
 def getPins(request, zoom, gridSize=512):
 
     clusterer = MapClusterer()
+    clustercells, filters = clusterer.getClusterParameters(request,int(zoom),int(gridSize))
 
-    markers = clusterer.kmeansCluster(request,int(zoom), int(gridSize))
+    markers = clusterer.kmeansCluster(clustercells,filters,int(zoom), int(gridSize))
     
     return HttpResponse(simplejson.dumps(
         markers
+        ), content_type="application/json")
+
+
+def getBounds(request,srid=4326):
+    clusterer = MapClusterer()
+    filters = clusterer.parseFilters(request)
+    filterstring = clusterer.constructFilterstring(filters)
+
+    bounds = clusterer.maptools.getBounds(filterstring,srid)
+
+    return HttpResponse(simplejson.dumps(
+        bounds
         ), content_type="application/json")
