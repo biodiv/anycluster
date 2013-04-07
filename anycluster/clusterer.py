1d0c857990a351bff0447a37a6cad396c3ba4291
biodiv <tom@eyesonic.net>
2013-04-07 21:18:41 +0200

    added getBounds, extracted filtering mechanism into own method

--- anycluster/clusterer.py
+++ anycluster/clusterer.py
@@ -20,13 +20,19 @@ DEBUG = False
 
 if DEBUG == False:
     from django.conf import settings
-    from django.db.models import Q
+    from django.db.models import Q, Min
     from django.db.models.loading import get_model
 
     #get the model as defined in settings
     geoapp, geomodel = settings.ANYCLUSTER_GEODJANGO_MODEL.split('.')
     geo_column_str = settings.ANYCLUSTER_COORDINATES_COLUMN
     CLUSTER_FILTERS = getattr(settings, 'ANYCLUSTER_FILTERS', None)
+    PINCOLUMN = getattr(settings, 'ANYCLUSTER_PINCOLUMN', None)
+
+    if PINCOLUMN:
+        pin_qry = [', MIN(%s) AS pinimg' %(PINCOLUMN) ,PINCOLUMN +',']
+    else:
+        pin_qry = ['','']
 
     Gis = get_model(geoapp,geomodel)
 
@@ -323,6 +329,41 @@ class MapTools():
         distance_p = distance_m / res
         
         return int(distance_p)
+
+
+
+    def getBounds(self, filterstring, output_srid=4326):
+
+        if filterstring:
+            filterstring = filterstring.lstrip(' AND ( ').replace(')', '', 1)
+            filterstring = ' WHERE ' + filterstring
+
+        #get the minimum bounding box. django cant handle Box() geometries, so we use ST_xmaxmin isntead of ST_Extent
+        envelope = Gis.objects.raw('''SELECT ST_xMin(ST_Collect(%s)) AS id, ST_xMax(ST_Collect(%s)) as xmax,
+                                        ST_yMin(ST_Collect(%s)) as ymin, ST_yMax(ST_Collect(%s)) as ymax FROM %s %s;''' %(geo_column_str,geo_column_str,
+                                                                                                                geo_column_str,geo_column_str,
+                                                                                                                geo_table,filterstring))
+        #convert values to latlng
+        srid_qry = "SELECT id, ST_SRID(%s) FROM %s LIMIT 1;" %(geo_column_str, geo_table)
+        srid_db = Gis.objects.raw(srid_qry)[0].st_srid
+        
+        xmin = envelope[0].id
+        xmax = envelope[0].xmax
+        ymin = envelope[0].ymin
+        ymax = envelope[0].ymax
+
+        #convert bounds to wanted srid
+        bottomleft = Point(xmin, ymin, srid=srid_db )
+        topright = Point(xmax, ymax, srid=srid_db)
+        self.point_AnyToAny(bottomleft,srid_db,output_srid)
+        self.point_AnyToAny(topright,srid_db,output_srid)
+
+        #google maps has fitbounds for bottomleft (sw) topright(ne) bounds
+        #openlayers has getZoomForExtent, so zoom/center calculation is not needed, bounds are enough
+        #furthermore, the clusterer should be able to display on all maps, so including tilesizes etc here
+        #would limit compatibility
+        
+        return {'left':bottomleft.x, 'top':topright.y, 'right':topright.x, 'bottom':bottomleft.y}
     
 
 #default is latlng 4326, input srid = output srid!
@@ -332,55 +373,69 @@ class MapClusterer():
 
         self.input_srid = input_srid
         self.maptools = MapTools(mapTileSize)
-        self.valid_operators = ['=','<','>','<=','>=']
+        self.valid_operators = ['=','<','>','<=','>=','list','!list']
 
+    #pythonifying the request
+    #this is optional and only necessary if you don't pass parameters directly from python
+    def parseRequest(self, request):
+        viewport = self.parseViewport(request)
+        filters = self.parseFilters(request)
+        return viewport,filters
 
-    #cache: {'filters':{}, 'cellIDs':[], 'zoom':1}, #returns clustercells and filters
-    def compareWithCache(self, request, clustercells, zoom):
+    def parseViewport(self,request):
         
-        clustercache = request.session.get('clustercache',{})
+        viewport = {'left':request.GET['left'], 'top':request.GET['top'],
+                    'right':request.GET['right'], 'bottom':request.GET['bottom']}
+        return viewport
 
-        deliver_cache = request.GET.get('cache', None)
-
+    def parseFilters(self,request):
+        
         filters = []
         
         if CLUSTER_FILTERS:
-                         
             for key in CLUSTER_FILTERS:
                 value_pre = request.GET.get(key, None)
-
                 if value_pre:
                     vwop = value_pre.split('_')
 
                     if len(vwop) == 2:
                         operator = vwop[0]
-                        value = vwop[1]
+                        values_string = vwop[1]
+                        values = values_string.split(',')
                         
-                        filters.append({'column':key, 'value':value, 'operator':operator})
+                        filters.append({'column':key, 'values':values, 'operator':operator})
                         
                     else:
-                        value = value_pre
+                        values = value_pre.split(',')
                         
-                        filters.append({'column':key, 'value':value})
+                        filters.append({'column':key, 'values':values})
 
+
+        return filters
+
+
+    #cache: {'filters':{}, 'cellIDs':[], 'zoom':1}, #returns clustercells
+    def compareWithCache(self, request, filters, clustercells, zoom):
         
+        clustercache = request.session.get('clustercache',{})
+
+        deliver_cache = request.GET.get('cache', None)
+
         new_clustercells = []
 
         use_cache = False
-
-        
 
         if clustercache and not deliver_cache:
             #clear cache if zoomlevel changed
             last_zoom = clustercache.get('zoom', None)
 
-            if zoom == last_zoom:
+            if int(zoom) == int(last_zoom):
 
-                applied_filters = clustercache.get('filters', {})
+                applied_filters = clustercache.get('filters', [])
             
                 if filters == applied_filters:
-                    
                     use_cache = True
+
 
         if use_cache:
             old_cells = clustercache['cellIDs']       
@@ -399,7 +454,8 @@ class MapClusterer():
 
         request.session['clustercache'] = clustercache
 
-        return new_clustercells, filters
+        return new_clustercells
+
         
     def constructFilterstring(self,filters):
         
@@ -408,8 +464,8 @@ class MapClusterer():
         for fltr in filters:
                            
 
-            #if there are multiple values, they are separated by a comma
-            values = fltr['value'].split(',')
+            #there can be multiple values
+            values = fltr['values']
             column = fltr['column']
             operator = fltr.get('operator', None)
 
@@ -419,14 +475,34 @@ class MapClusterer():
 
                 if column == 'time':
 
+                    if operator is None or operator == 'seq':
+                        days = values[0].split('-')
+                        months = values[1].split('-')
+                        years = values[2].split('-')
+
+
                     if operator is not None:
+
+                        if operator in self.valid_operators:
                         
-                        filterstring += "%s %s TIMESTAMP '%s'" %(column, operator, values[0])
+                            filterstring += "%s %s TIMESTAMP '%s'" %(column, operator, values[0])
+
+
+                        elif operator == 'range':
+                            filterstring += "%s >= TIMESTAMP '%s' AND %s <= TIMESTAMP '%s' " %(column, values[0], column, values[1])
+
+
+                        elif operator == 'seq':
+                            
+                            filterstring += '''EXTRACT(YEAR FROM time) >= %s
+                                            AND EXTRACT(YEAR FROM time) <= %s
+                                            AND EXTRACT(MONTH FROM time) >= %s
+                                            AND EXTRACT(MONTH FROM time) <= %s
+                                            '''%(years[0],years[1],months[0],months[1])
+                            
+                            
 
                     else:
-
-                        months = values[0].split('-')
-                        years = values[1].split('-')
 
                         filterstring += '''EXTRACT(YEAR FROM time) >= %s
                                             AND EXTRACT(YEAR FROM time) <= %s
@@ -436,20 +512,41 @@ class MapClusterer():
                     
                 else:
 
-                    valcounter = 0
-
-                    for val in values:
-                        if valcounter > 0:
-                            filterstring += ' OR '
-
-                        if operator and operator in self.valid_operators:
-                            filterstring += "%s %s '%s' " %(column, operator, val)
+                    if operator == 'list' or operator == '!list':
+                        if operator == 'list':
+                            operator = 'IN'
                         else:
-                            filterstring += "%s ~ '^%s.*' " %(column, val)
+                            operator = 'NOT IN'
+                            
+                        filterstring += ' %s %s (' %(column,operator)
+
+                        first = True
+                        for val in values:
+                            if first:
+                                filterstring += "'%s'" %val
+                                first = False
+                            else:
+                                filterstring += ",'%s'" %val
+
+                        filterstring += ')'
                         
+
+                    else:
+                    
+                        valcounter = 0
+
+                        for val in values:
+                            if valcounter > 0:
+                                filterstring += ' OR '
+
+                            if operator and operator in self.valid_operators:
+                                filterstring += "%s %s '%s' " %(column, operator, val)
+                            else:
+                                filterstring += "%s ~ '^%s.*' " %(column, val)
                             
-                            
-                        valcounter += 1
+                                
+                                
+                            valcounter += 1
 
                 filterstring += ')'
 
@@ -457,10 +554,9 @@ class MapClusterer():
         
 
     #merge near clusters after kmeans [{'id':z, 'count':y, 'coordinates':x}], very simple method
-    def distanceCluster(self, points, zoom):
+    def distanceCluster(self, points, zoom, c_distance=50):
 
         #clusterdistance in pixels, as this is constant on every zoom level
-        c_distance = 50
 
         current_clist = points #[ point for point in clusters]
 
@@ -500,18 +596,12 @@ class MapClusterer():
                     break
 
 
-        return current_clist                 
+        return current_clist
     
 
     #defaults to goole srid
     #viewport is {'top':1,'right':2,'bottom':3,'left':4}
-    def gridCluster(self, request, zoom, gridSize = 256, **kwargs):
-
-        viewport = request.GET
-                
-        clustercells_pre = self.getClusterCells(viewport, zoom, gridSize)
-
-        clustercells, filters = self.compareWithCache(request, clustercells_pre, zoom)
+    def gridCluster(self, clustercells, filters, zoom, gridSize = 256, **kwargs):
 
         if DEBUG:
             print('clustercells: %s' %clustercells)
@@ -537,11 +627,22 @@ class MapClusterer():
 
                 filterstring = self.constructFilterstring(filters)
 
-                pin_count_pre = Gis.objects.raw(''' SELECT COUNT(*) as id FROM %s WHERE ST_Within(%s, ST_GeomFromText('%s',%s) )
+                pin_count_pre = Gis.objects.raw(''' SELECT COUNT(*) AS id FROM %s WHERE ST_Within(%s, ST_GeomFromText('%s',%s) )
                                                 %s                                                
                                             ''' %(geo_table ,geo_column_str, poly, srid_db, filterstring) )
 
                 pin_count = int(pin_count_pre[0].id)
+
+                if PINCOLUMN is not None and pin_count == 1:
+                    pinimg_pre = Gis.objects.raw(''' SELECT %s AS id, %s AS coordinates FROM %s WHERE ST_Within(%s, ST_GeomFromText('%s',%s) )
+                                                %s                                                
+                                            ''' %(PINCOLUMN, geo_column_str, geo_table ,geo_column_str, poly, srid_db, filterstring) )
+
+                    pinimg = pinimg_pre[0].id
+                    coordinates = pinimg_pre[0].coordinates
+                else:
+                    pinimg = None
+                
                 #django orm fails on range of months across years, use raw if filters are applies
                 '''
                 for fltr in filters:
@@ -580,8 +681,24 @@ class MapClusterer():
                     Q_filters.add(E_filters, Q.AND)
             '''
             else:
-                pin_count = Gis.objects.filter(Q_filters).count()
+                if PINCOLUMN:
+                    pin_count = Gis.objects.filter(Q_filters).count()
+                    
+                    if pin_count == 1:
+                        pinimg_pre = Gis.objects.filter(Q_filters)[0]
+                        pinimg = getattr(pinimg_pre,PINCOLUMN)
+                        coordinates = getattr(pinimg_pre,geo_column_str)
+                    else:
+                        pinimg = None
+                    
 
+                else:
+                    pin_count = Gis.objects.filter(Q_filters).count()
+                    pinimg = None
+
+
+                
+                    
             #transform the polys to output srid if necessary
             if srid_db != self.input_srid:
                 self.maptools.point_AnyToAny(cell_topright, srid_db, self.input_srid)
@@ -594,12 +711,22 @@ class MapClusterer():
             nodes = [{x: cell_topright.x, y: cell_topright.y}, {x:cell_topright.x, y:cell_bottomleft.y},
                     {x: cell_bottomleft.x, y: cell_bottomleft.y}, {x: cell_bottomleft.x, y: cell_topright.y}]
 
-            center_x = (cell_topright.x + cell_bottomleft.x)/2
-            center_y = (cell_topright.y + cell_bottomleft.y)/2
+            if int(pin_count) == 1 and PINCOLUMN is not None:
+                center_pre = Point(coordinates.x,coordinates.y, srid=srid_db)
+                
+                if srid_db != self.input_srid:
+                    self.maptools.point_AnyToAny(center_pre, srid_db, self.input_srid)
+                    
+                center_x = center_pre.x
+                center_y = center_pre.y
+
+            else:
+                center_x = (cell_topright.x + cell_bottomleft.x)/2
+                center_y = (cell_topright.y + cell_bottomleft.y)/2
             
             center = {x: center_x, y:center_y}
 
-            cellobj = {'cell':nodes, 'count': pin_count, 'center':center}
+            cellobj = {'cell':nodes, 'count': pin_count, 'center':center, 'pinimg':pinimg}
 
             if DEBUG:
                 print('\n\noutput: %s ' %nodes)
@@ -607,15 +734,21 @@ class MapClusterer():
             gridCells.append(cellobj)
 
         return gridCells
+
+
+    def getClusterParameters(self, request, zoom, gridSize):
+        
+        viewport, filters = self.parseRequest(request)
+        #get the clustering cells
+        clustercells_pre = self.getClusterCells(viewport, zoom, gridSize)
+        
+        #clean the cells
+        clustercells = self.compareWithCache(request, filters, clustercells_pre, zoom)
+        
+        return clustercells, filters
     
 
-    def kmeansCluster(self, request, zoom, gridSize=512, **kwargs):
-
-        viewport = request.GET
-        
-        clustercells_pre = self.getClusterCells(viewport, zoom, gridSize)
-
-        clustercells, filters = self.compareWithCache(request, clustercells_pre, zoom)
+    def kmeansCluster(self, clustercells, filters, zoom, gridSize=512, **kwargs):
 
         pins = []
 
@@ -630,28 +763,28 @@ class MapClusterer():
 
                 #ST_AsText( ST_MinimumBoundingCircle(ST_Collect(%s),3) ) AS nodes
                 cellpins = Gis.objects.raw(
-                        '''SELECT kmeans AS id, count(*), ST_Centroid(ST_Collect(%s)) AS coordinates
+                        '''SELECT kmeans AS id, count(*), ST_Centroid(ST_Collect(%s)) AS coordinates %s
                             FROM (
-                              SELECT kmeans(ARRAY[ST_X(%s), ST_Y(%s)], 6) OVER (), %s
+                              SELECT %s kmeans(ARRAY[ST_X(%s), ST_Y(%s)], 6) OVER (), %s
                               FROM %s
                               WHERE ST_Within(%s, ST_GeomFromText('%s',%s) ) %s
                             ) AS ksub
                             GROUP BY kmeans
                             ORDER BY kmeans;
-                        ''' %(geo_column_str,geo_column_str,geo_column_str,geo_column_str, geo_table,geo_column_str, poly, srid_db, filterstring)
+                        ''' %(geo_column_str,pin_qry[0],pin_qry[1],geo_column_str,geo_column_str,geo_column_str, geo_table,geo_column_str, poly, srid_db, filterstring)
                         )
 
             else:
 
-                cellpins = Gis.objects.raw('''SELECT kmeans AS id, count(*), ST_Centroid(ST_Collect(%s)) AS coordinates
+                cellpins = Gis.objects.raw('''SELECT kmeans AS id, count(*), ST_Centroid(ST_Collect(%s)) AS coordinates %s
                             FROM (
-                              SELECT kmeans(ARRAY[ST_X(%s), ST_Y(%s)], 6) OVER (), %s
+                              SELECT %s kmeans(ARRAY[ST_X(%s), ST_Y(%s)], 6) OVER (), %s
                               FROM %s
                               WHERE ST_Within(%s, ST_GeomFromText('%s',%s) )
                             ) AS ksub
                             GROUP BY kmeans
                             ORDER BY kmeans;
-                        ''' %(geo_column_str,geo_column_str,geo_column_str,geo_column_str, geo_table,geo_column_str, poly, srid_db)
+                        ''' %(geo_column_str,pin_qry[0],pin_qry[1],geo_column_str,geo_column_str,geo_column_str, geo_table,geo_column_str, poly, srid_db)
                         )
 
             #clean the clusters
@@ -699,8 +832,13 @@ class MapClusterer():
                 '''
                 if point.srid != self.input_srid:
                     self.maptools.point_AnyToAny(point, point.srid, self.input_srid)
+
+                if PINCOLUMN:
+                    pinimg = cell.pinimg
+                else:
+                    pinimg = None
                 
-                pins.append({'count':cell.count, 'center':{'x':point.x, 'y':point.y}})
+                pins.append({'count':cell.count, 'center':{'x':point.x, 'y':point.y}, 'pinimg':pinimg})
 
         return pins
         
