1c0df4ad9516302d0066b0a0394fa6691908ce8e
Shevchenko Vitaliy <vetal4444@gmail.com>
2014-06-04 20:41:24 +0300

    Fix parseFilters usage in getClusterContent. Remove getBounds view.

--- anycluster/urls.py
+++ anycluster/urls.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 from django.conf.urls import patterns, url
 from anycluster import views
 from django.conf import settings
@@ -8,8 +5,6 @@ from django.conf import settings
 urlpatterns = patterns('',
     url(r'^grid/(\d+)/(\d+)/$', views.getGrid, name='getGrid'),
     url(r'^kmeans/(\d+)/(\d+)/$', views.getPins, name='getPins'),
-    url(r'^getbounds/$', views.getBounds, name='getBounds'),
-    url(r'^getbounds/(\d+)/$', views.getBounds, name='getBounds_wsrid'),
     url(r'^getClusterContent/(\d+)/(\d+)/$', views.getClusterContent, name='getClusterContent'),
     url(r'^getAreaMarkers/(\d+)/(\d+)/$', views.getAreaMarkers, name='getAreaMarkers'),
     url(r'^getViewportMarkers/(\d+)/(\d+)/$', views.getViewportMarkers, name='getViewportMarkers'),
