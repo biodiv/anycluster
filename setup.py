ed95c3e1fd3abaffd1da3a8eff291161027af389
Claudiu Ivan <contact@claudiu-ivan.com>
2014-09-20 18:31:03 +0300

    added setup.py, manifest file and a license, still need to complete author information and validate license type

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'django',
+]
+
+setup(
+    name="django-anycluster",
+    version='0.1',
+    description='anycluster provides Server-Side clustering of map markers for Geodjango',
+    license='The MIT License',
+    platforms=['OS Independent'],
+    keywords='django, cluster, k-means, grid',
+    author='biodiv',
+    author_email='biodiv@email.com',
+    url='https://github.com/biodiv/anycluster',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+)
