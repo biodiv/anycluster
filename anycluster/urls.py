9a49ce93428d6e7bdfeebbed906a1868dd844169
Sylvain BLOT <sylvainblot@me.com>
2017-03-27 21:11:10 +0200

    Update url format to support Django 1.10

--- anycluster/urls.py
+++ anycluster/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from anycluster import views
 from django.conf import settings
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^grid/(\d+)/(\d+)/$', views.getGrid, name='getGrid'),
     url(r'^kmeans/(\d+)/(\d+)/$', views.getPins, name='getPins'),
     url(r'^getClusterContent/(\d+)/(\d+)/$', views.getClusterContent, name='getClusterContent'),
     url(r'^getAreaContent/(\d+)/(\d+)/$', views.getAreaContent, name='getAreaContent'),
-)
+]
